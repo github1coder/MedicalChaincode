45c14df858ba7bb325143d6e3bfb93d139a5b3f4
github1coder <2582448456@qq.com>
2024-03-20 22:10:05 +0800

    backend

--- MedicalChaincode/main/model.go
+++ MedicalChaincode/main/model.go
@@ -1,9 +1,6 @@
 package main
 
 // 导入所需的包
-import (
-	// "time"
-)
 
 // 医疗记录
 // type MedicalRecord struct {
@@ -15,44 +12,44 @@ import (
 // 结构体各个成员的涵义、类型都有待商榷，没有仔细看过数据
 // 第一列我没加进来，列名为空不知道什么涵义
 type MedicalRecord struct {
-	index                string               `json:"index"`                // 0
-	SUBJECT_ID           string               `json:"SUBJECT_ID"`           //249
-	GENDER               string            		`json:"GENDER"`               // F
-	DOB                  string         			`json:"DOB"`                  // 2075-03-13 00:00:00
-	DOD                  string         			`json:"DOD"`                  // NaN
-	DOD_HOSP             string         			`json:"DOD_HOSP"`             // NaN
-	DOD_SSN              string         			`json:"DOD_SSN"`              // NaN
-	EXPIRE_FLAG          string               `json:"EXPIRE_FLAG"`          // 0
-	HADM_ID              string               `json:"HADM_ID"`              // 患者住院期间的唯一标识； 若为门诊患者则字段为空 149546
-	ADMITTIME            string         			`json:"ADMITTIME"`            // 2155-02-03 20:16:00
-	DISCHTIME            string         			`json:"DISCHTIME"`            // 2155-02-03 20:16:00
-	DEATHTIME            string         			`json:"DEATHTIME"`            // NaN
-	ADMISSION_TYPE       string            		`json:"ADMISSION_TYPE"`       // EMERGENCY
-	ADMISSION_LOCATION   string            		`json:"ADMISSION_LOCATION"`   // EMERGENCY ROOM ADMIT
-	DISCHARGE_LOCATION   string            		`json:"DISCHARGE_LOCATION"`   // REHAB/DISTINCT PART HOSP
-	INSURANCE            string            		`json:"INSURANCE"`            // Medicare
-	LANGUAGE             string            		`json:"LANGUAGE"`             // ENGL
-	RELIGION             string            		`json:"RELIGION"`             // CATHOLIC
-	MARITAL_STATUS       string            		`json:"MARITAL_STATUS"`       // DIVORCED
-	ETHNICITY            string            		`json:"ETHNICITY"`            // WHITE
-	EDREGTIME            string         			`json:"EDREGTIME"`            // 2155-02-03 17:43:00
-	EDOUTTIME            string         			`json:"EDOUTTIME"`            // 2155-02-03 17:43:00
-	DIAGNOSIS            string            		`json:"DIAGNOSIS"`            // GI BLEED/ CHEST PAIN
-	HOSPITAL_EXPIRE_FLAG string               `json:"HOSPITAL_EXPIRE_FLAG"` // 0
-	HAS_CHARTEVENTS_DATA string               `json:"HAS_CHARTEVENTS_DATA"` // 1
-	ICUSTAY_DETAILS      string 							`json:"ICUSTAY_DETAILS"`      // ICUSTAY_ID: 269035, FIRST_CAREUNIT: MICU, LAST...
-	ICD9_CODE            string          			`json:"ICD9_CODE"`            // 包含了这个患者这次住院所有的症状代码（和其他对应信息） //ICD-9是一套编码系统，用于标识各种疾病、疾病症状、损伤和其他健康相关情况 // ['56985', '41071', '43411', '5849', '2851', '3...
-	PRO_CODE             string           	  `json:"PRO_CODE"`             // [3974, 40, 8841, 9910]
-	Drug_Details         string 							`json:"Drug_Details"`         //  ICUSTAY_ID: 208413, DRUG: 0.9% Sodium Chloride...
-	Input_Details        string 							`json:"Input_Details"`        //  ICUSTAY_ID: 269035.0, STARTTIME: 2155-02-05 17...
-	Note_Details         string            		`json:"Note_Details"`         //  1495461495461495461495461495461495461495461495...
+	index                string `json:"index"`                // 0
+	SUBJECT_ID           string `json:"SUBJECT_ID"`           //249
+	GENDER               string `json:"GENDER"`               // F
+	DOB                  string `json:"DOB"`                  // 2075-03-13 00:00:00
+	DOD                  string `json:"DOD"`                  // NaN
+	DOD_HOSP             string `json:"DOD_HOSP"`             // NaN
+	DOD_SSN              string `json:"DOD_SSN"`              // NaN
+	EXPIRE_FLAG          string `json:"EXPIRE_FLAG"`          // 0
+	HADM_ID              string `json:"HADM_ID"`              // 患者住院期间的唯一标识； 若为门诊患者则字段为空 149546
+	ADMITTIME            string `json:"ADMITTIME"`            // 2155-02-03 20:16:00
+	DISCHTIME            string `json:"DISCHTIME"`            // 2155-02-03 20:16:00
+	DEATHTIME            string `json:"DEATHTIME"`            // NaN
+	ADMISSION_TYPE       string `json:"ADMISSION_TYPE"`       // EMERGENCY
+	ADMISSION_LOCATION   string `json:"ADMISSION_LOCATION"`   // EMERGENCY ROOM ADMIT
+	DISCHARGE_LOCATION   string `json:"DISCHARGE_LOCATION"`   // REHAB/DISTINCT PART HOSP
+	INSURANCE            string `json:"INSURANCE"`            // Medicare
+	LANGUAGE             string `json:"LANGUAGE"`             // ENGL
+	RELIGION             string `json:"RELIGION"`             // CATHOLIC
+	MARITAL_STATUS       string `json:"MARITAL_STATUS"`       // DIVORCED
+	ETHNICITY            string `json:"ETHNICITY"`            // WHITE
+	EDREGTIME            string `json:"EDREGTIME"`            // 2155-02-03 17:43:00
+	EDOUTTIME            string `json:"EDOUTTIME"`            // 2155-02-03 17:43:00
+	DIAGNOSIS            string `json:"DIAGNOSIS"`            // GI BLEED/ CHEST PAIN
+	HOSPITAL_EXPIRE_FLAG string `json:"HOSPITAL_EXPIRE_FLAG"` // 0
+	HAS_CHARTEVENTS_DATA string `json:"HAS_CHARTEVENTS_DATA"` // 1
+	ICUSTAY_DETAILS      string `json:"ICUSTAY_DETAILS"`      // ICUSTAY_ID: 269035, FIRST_CAREUNIT: MICU, LAST...
+	ICD9_CODE            string `json:"ICD9_CODE"`            // 包含了这个患者这次住院所有的症状代码（和其他对应信息） //ICD-9是一套编码系统，用于标识各种疾病、疾病症状、损伤和其他健康相关情况 // ['56985', '41071', '43411', '5849', '2851', '3...
+	PRO_CODE             string `json:"PRO_CODE"`             // [3974, 40, 8841, 9910]
+	Drug_Details         string `json:"Drug_Details"`         //  ICUSTAY_ID: 208413, DRUG: 0.9% Sodium Chloride...
+	Input_Details        string `json:"Input_Details"`        //  ICUSTAY_ID: 269035.0, STARTTIME: 2155-02-05 17...
+	Note_Details         string `json:"Note_Details"`         //  1495461495461495461495461495461495461495461495...
 }
 
 type TxnRecord struct {
-	index								 string								`json:"index"`
-	FUNCTION             string               `json:"function_invoked"`
-	PARAMETER            string               `json:"function_parameter_value"`
-	ClientMSPID          string               `json:"client_MSP_id"`  // 发布事务的节点客户端的MSPID
-	TIME                 string               `json:"time"`
-	SUCCESS              string               `json:"submiting_commit_abort"`
+	index       string `json:"index"`
+	FUNCTION    string `json:"function_invoked"`
+	PARAMETER   string `json:"function_parameter_value"`
+	ClientMSPID string `json:"client_MSP_id"` // 发布事务的节点客户端的MSPID
+	TIME        string `json:"time"`
+	SUCCESS     string `json:"submiting_commit_abort"`
 }
--- MedicalChaincode/main/transaction.go
+++ MedicalChaincode/main/transaction.go
@@ -11,56 +11,56 @@ import (
 )
 
 // 提交事务
-func (mc *MedicalChaincode) SubmitTransaction(ctx contractapi.TransactionContextInterface, 
+func (mc *MedicalChaincode) SubmitTransaction(ctx contractapi.TransactionContextInterface,
 	index string, clientMSPID string, funcType string, paraVal string, commit string) (*TxnRecord, error) {
-		// 提交区块前数据变更不会体现在区块中，因此同一个区块内的重复事务变更不会冲突
-		txnRecordJSON, _ := ctx.GetStub().GetState("txn"+index)
-		if txnRecordJSON != nil {
-			return nil, fmt.Errorf("重复提交事务: %s", index)
-		}
-	
-		currentTime := time.Now()
-		txnRecord := TxnRecord {
-			index:      	index,
-			FUNCTION:   	funcType,
-			PARAMETER:  	paraVal,
-			ClientMSPID:  clientMSPID,
-			TIME:         currentTime.Format("2006-01-02 15:04:05"),
-			SUCCESS:      "submiting or aborted",
-		}
-		// 目前尚不能完成事务状态的转变，因为如果失败会直接挂掉、之前写入的submiting事务也不会被写入区块
-		if commit == "commit" {
-			txnRecord.SUCCESS = "commited"
-		}
+	// 提交区块前数据变更不会体现在区块中，因此同一个区块内的重复事务变更不会冲突
+	txnRecordJSON, _ := ctx.GetStub().GetState("txn" + index)
+	if txnRecordJSON != nil {
+		return nil, fmt.Errorf("重复提交事务: %s", index)
+	}
 
-		RecordJSON, err := json.Marshal(txnRecord)
-		if err != nil {
-			return nil, fmt.Errorf("转换json失败: %v", err)
-		}
-		err = ctx.GetStub().PutState("txn"+txnRecord.index, RecordJSON)
-		if err != nil {
-			return nil, fmt.Errorf("存入状态数据库失败: %v", err)
-		}
+	currentTime := time.Now()
+	txnRecord := TxnRecord{
+		index:       index,
+		FUNCTION:    funcType,
+		PARAMETER:   paraVal,
+		ClientMSPID: clientMSPID,
+		TIME:        currentTime.Format("2006-01-02 15:04:05"),
+		SUCCESS:     "submiting or aborted",
+	}
+	// 目前尚不能完成事务状态的转变，因为如果失败会直接挂掉、之前写入的submiting事务也不会被写入区块
+	if commit == "commit" {
+		txnRecord.SUCCESS = "commited"
+	}
 
-		return &txnRecord, nil
+	RecordJSON, err := json.Marshal(txnRecord)
+	if err != nil {
+		return nil, fmt.Errorf("转换json失败: %v", err)
+	}
+	err = ctx.GetStub().PutState("txn"+txnRecord.index, RecordJSON)
+	if err != nil {
+		return nil, fmt.Errorf("存入状态数据库失败: %v", err)
+	}
+
+	return &txnRecord, nil
 }
 
 // 读取事务
 func (mc *MedicalChaincode) GetTxnRecord(ctx contractapi.TransactionContextInterface, index string) (*TxnRecord, error) {
-	txnRecordJSON, err := ctx.GetStub().GetState("txn"+index)
-		if err != nil {
-			return nil, fmt.Errorf("读取事务数据库失败: %v", err)
-		}
+	txnRecordJSON, err := ctx.GetStub().GetState("txn" + index)
+	if err != nil {
+		return nil, fmt.Errorf("读取事务数据库失败: %v", err)
+	}
 
-		if txnRecordJSON == nil {
-			return nil, fmt.Errorf("不存在该事务记录： %s", index)
-		}
+	if txnRecordJSON == nil {
+		return nil, fmt.Errorf("不存在该事务记录： %s", index)
+	}
 
-		var txnRecord TxnRecord
-		err = json.Unmarshal(txnRecordJSON, &txnRecord)
-		if err != nil {
-			return nil, fmt.Errorf("解析事务记录json失败: %v", err)
-		}
+	var txnRecord TxnRecord
+	err = json.Unmarshal(txnRecordJSON, &txnRecord)
+	if err != nil {
+		return nil, fmt.Errorf("解析事务记录json失败: %v", err)
+	}
 
-		return &txnRecord, nil
+	return &txnRecord, nil
 }
